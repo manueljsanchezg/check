c164d5709fdb77ba952356beee1ff8d31d872247
Fernando Fernández <ferferga@hotmail.com>
2023-11-08 08:59:13 +0100

    test2: creacion entidad coupon

--- src/main/java/org/springframework/samples/petclinic/grooming/Coupon.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/Coupon.java
@@ -3,18 +3,30 @@ package org.springframework.samples.petclinic.grooming;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.visit.Visit;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Coupon {
+@Entity
+public class Coupon extends BaseEntity {
     
+    @Column(name = "start")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     LocalDate startDate;
     
+    @Column(name = "finish")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     LocalDate expiryDate;
     
     @Transient
