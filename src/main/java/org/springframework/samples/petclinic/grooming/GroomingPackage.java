14a39ec4e2d170d57e0355117ac34fb5ebff56f8
Fernando Fernández <ferferga@hotmail.com>
2023-11-08 08:52:39 +0100

    test1: groomingpackage

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackage.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackage.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-
+@Entity
 public class GroomingPackage extends BaseEntity {
+    @DecimalMin("0.01")
+    @NotNull
     double cost;
 
+    @NotNull
+    @NotEmpty
     String description;
 
     @Transient
