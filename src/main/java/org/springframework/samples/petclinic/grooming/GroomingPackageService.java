003563454f4134f9d4656c1dc1f9e22eb5a829df
Fernando Fernández <ferferga@hotmail.com>
2023-11-08 10:10:50 +0100

    test6

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackageService.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackageService.java
@@ -2,21 +2,25 @@ package org.springframework.samples.petclinic.grooming;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 public class GroomingPackageService {
     GroomingPackageRepository gr;
    
+    @Autowired
     public GroomingPackageService(GroomingPackageRepository gr){
         this.gr=gr;
     }
 
+    @Transactional
     public GroomingPackage save(GroomingPackage o) {
-        // TODO: Change this
-        return null;
+        return gr.save(o);
     }
     
+    @Transactional(readOnly = true)
     public List<GroomingPackage> getAll() {
-        // TODO: Change this
-        return null;
+        return gr.findAll();
     }
     
 }
