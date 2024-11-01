6a79497c6d6135979a327aedc1cedf1a7562d33b
manueljsanchezg <manueljsanchez10@gmail.com>
2024-11-01 20:17:50 +0100

    Ejercicio 2

--- src/main/java/org/springframework/samples/petclinic/grooming/Coupon.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/Coupon.java
@@ -3,18 +3,31 @@ package org.springframework.samples.petclinic.grooming;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.visit.Visit;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Coupon {
+@Entity
+public class Coupon extends BaseEntity {
     
+    
+    @NotNull
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "start")
     LocalDate startDate;
     
+    @NotNull
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "finsish")
     LocalDate expiryDate;
     
     @Transient
--- src/main/java/org/springframework/samples/petclinic/grooming/CouponRepository.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/CouponRepository.java
@@ -3,7 +3,9 @@ package org.springframework.samples.petclinic.grooming;
 import java.util.List;
 import java.util.Optional;
 
-public interface CouponRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface CouponRepository extends CrudRepository<Coupon, Integer> {
     Coupon save(Coupon o);
     List<Coupon> findAll();
     Optional<Coupon> findById(Integer id);
