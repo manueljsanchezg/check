cbdbb74b8f04074967a68ad475b134f4678b616d
manueljsanchezg <manueljsanchez10@gmail.com>
2024-11-01 20:14:15 +0100

    Ejercicio 1

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackage.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackage.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-
+@Entity
 public class GroomingPackage extends BaseEntity {
+    
+    @NotNull
+    @Positive
     double cost;
 
+    @NotEmpty
     String description;
 
     @Transient
