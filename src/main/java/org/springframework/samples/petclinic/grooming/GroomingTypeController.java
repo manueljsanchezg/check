9f8eace2784e464b157ca9848310f285ede22a03
manueljsanchezg <manueljsanchez10@gmail.com>
2024-11-01 21:39:51 +0100

    Ejercicio 9* El anterior es el 8

--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingTypeController.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import jakarta.validation.Valid;
 
@@ -39,6 +42,10 @@ public class GroomingTypeController {
         return new ResponseEntity<>(gt, HttpStatus.OK);
     }
 
-    
+    @PostMapping
+    public ResponseEntity<GroomingType> create( @Valid @RequestBody GroomingType gt) {
+        GroomingType saved = gts.save(gt);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
     
 }
