5e366531c485de8efb664bc1203f1b099482d587
manueljsanchezg <manueljsanchez10@gmail.com>
2024-11-01 20:50:07 +0100

    Ejercicio 6

--- src/main/java/org/springframework/samples/petclinic/grooming/CouponService.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/CouponService.java
@@ -2,24 +2,28 @@ package org.springframework.samples.petclinic.grooming;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.visit.Visit;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Service
 public class CouponService {
     private CouponRepository cr;
 
+    @Autowired
     public CouponService(CouponRepository cr){
         this.cr=cr;
     }
 
+    @Transactional
     public Coupon save(Coupon d) {
-        // TODO: Change this
-        return null;
+        return cr.save(d);
     }
     
+    @Transactional(readOnly = true)
     public List<Coupon> getAll() {
-        // TODO: Change this
-        return null;
+        return cr.findAll();
     }    
 
     public void addVisit(Coupon c, Visit v) throws UnfeasibleCouponException {
--- src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackageService.java
+++ src/main/java/org/springframework/samples/petclinic/grooming/GroomingPackageService.java
@@ -2,21 +2,26 @@ package org.springframework.samples.petclinic.grooming;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class GroomingPackageService {
     GroomingPackageRepository gr;
    
+    @Autowired
     public GroomingPackageService(GroomingPackageRepository gr){
         this.gr=gr;
     }
 
+    @Transactional
     public GroomingPackage save(GroomingPackage o) {
-        // TODO: Change this
-        return null;
+        return gr.save(o);
     }
-    
+    @Transactional(readOnly = true)
     public List<GroomingPackage> getAll() {
-        // TODO: Change this
-        return null;
+        return gr.findAll();
     }
     
 }
